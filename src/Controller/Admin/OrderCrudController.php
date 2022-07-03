d8856d29f1df5bd6d97adb42d22f9aaa5a79d7c2
Sinex <59825098+Floriansalducci@users.noreply.github.com>
2022-07-03 03:45:38 +0200

    Add gestion order statut with EeasyAdmin

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller\Admin;
 
+
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Order;
+
 
 class OrderCrudController extends AbstractCrudController
 {
+    private $em;
+    private $crudUrlGenerator;
+    private $adminUrlGenerator;
+
+    public function __construct(EntityManagerInterface $em,CrudUrlGenerator $crudUrlGenerator , AdminUrlGenerator $adminUrlGenerator )
+    {
+        $this->em = $em;
+        $this->crudUrlGenerator = $crudUrlGenerator;
+        $this->adminUrlGenerator = $adminUrlGenerator;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Order::class;
@@ -22,9 +41,48 @@ class OrderCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
+        $updatePreparation = Action::new('updatePreparation', 'Preparation en cours','fas fa-box-open')
+            ->linkToCrudAction('updatePreparation');
+        $updateDelivery = Action::new('updateDelivery', 'Livraison en cours','fa fa-truck')
+            ->linkToCrudAction('updateDelivery');
         return $actions
+            ->add('detail',$updatePreparation)
+            ->add('detail',$updateDelivery)
             ->add('index','detail' );
     }
+
+    public function updatePreparation(AdminContext $context)
+    {
+        $order = $context->getEntity()->getInstance();
+        $order->setState(2);
+        $this->em->flush();
+
+        $this->addFlash('notice', "<span style='color: green'>
+        <strong>La commande ".$order->getReference()."est bien en cours de préparation</u>.</strong></span>");
+
+        $url = $this->adminUrlGenerator
+            ->setController(OrderCrudController::class)
+            ->setAction('index')
+            ->generateUrl();
+        return $this->redirect($url);
+    }
+
+    public function updateDelivery(AdminContext $context)
+    {
+        $order = $context->getEntity()->getInstance();
+        $order->setState(3);
+        $this->em->flush();
+
+        $this->addFlash('notice', "<span style='color: orange'>
+        <strong>La commande ".$order->getReference()."est bien en cours de Livraison</u>.</strong></span>");
+
+        $url = $this->adminUrlGenerator
+            ->setController(OrderCrudController::class)
+            ->setAction('index')
+            ->generateUrl();
+        return $this->redirect($url);
+    }
+
 
     public function configureCrud(Crud $crud): Crud
     {
@@ -37,11 +95,18 @@ class OrderCrudController extends AbstractCrudController
             IdField::new('id'),
             DateTimeField::new('createdAt', 'Date de la commande'),
             TextField::new('user.fullname', 'Nom du client'),
+            TextEditorField::new('delivery', 'Adresse de livraison')->onlyOnDetail(),
             MoneyField::new('total', 'Total des produits')->setCurrency('EUR'),
             TextField::new('carrierName', 'Transporteur'),
             MoneyField::new('carrierPrice','Frais de port')->setCurrency('EUR'),
-            BooleanField::new('isPaid', 'Payé'),
+            ChoiceField::new('state')->setChoices([
+                'Non payé' => 0,
+                'Payé' => 1,
+                'Préparation en cours' => 2,
+                'Livraison en cours' => 3,
+            ]),
             ArrayField::new('orderDetails', 'Produits achetés')->hideOnIndex(),
+
 
 
         ];
